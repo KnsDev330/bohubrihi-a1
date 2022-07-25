07eafecee26af72a86985d4b20e9dbf59e4435c9
Khandaker Sajjat <me@kns.dev>
2022-07-25 16:23:38 +0600

    added some files

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const PRODUCTS = [
+    { name: "Deepcool RF 120R", price: 6400, img: "1.jpg" },
+    { name: "Deepcool RF 120W", price: 5400, img: "2.jpg" },
+    { name: "PCCooler RF 120X", price: 7300, img: "3.jpg" },
+    { name: "No Brand PC Cooler", price: 4600, img: "4.jpg" },
+    { name: "Apple PC Fan XSX", price: 9700, img: "5.jpg" },
+];
+
+const productsDiv = document.getElementById('products');
