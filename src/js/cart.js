e8b6208849922a46fe0fb5b99d6beac440f3ca37
Khandaker Sajjat <me@kns.dev>
2022-09-30 14:28:35 +0600

    Completed

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,27 +1,93 @@
+/* Products to work with */
+const PRODUCTS = [
+    { id: "UiPQXFZBKY", name: "Deepcool RF 120R", price: 6400, img: "1.jpg" },
+    { id: "oIyurusmMa", name: "No Brand PC Cooler", price: 4600, img: "4.jpg" },
+    { id: "YwuNwIetjx", name: "Apple PC Fan XSX", price: 9700, img: "5.jpg" },
+    { id: "xxuNwIetxx", name: "OnePlus PC Fan", price: 9999, img: "3.jpg" },
+];
+
+/* Gettings necessary Divs */
+const cartDiv = document.getElementById('cart');
+const clearCartBtn = document.getElementById('priceRmv');
+const priceDiv = document.getElementById('total-price');
+
 class Cart {
-    constructor(items = []) {
-        this.items = [];
+    constructor() {
+        this.items = JSON.parse(localStorage.getItem('cart')) || {};
     }
-    getItems() {
-        return this.items;
+    add(id) { // adds new item to the cart
+        if (this.items[id]) this.items[id] += 1;
+        else this.items[id] = 1;
+        localStorage.setItem('cart', JSON.stringify(this.items));
+        this.show();
     }
-    addItem(id) {
-        this.loadItems();
-        const items = this.items;
-        if (items[id]) {
-            items[id] += 1;
-        } else {
-            items[id] = 1;
+    clear() { // empty out the cart
+        this.items = {};
+        localStorage.removeItem('cart');
+        this.show();
+    }
+    remove(id) { // remove single item from cart by id
+        const item = PRODUCTS.find(v => v.id === id);
+        if (!item) return;
+        delete this.items[id];
+        localStorage.setItem('cart', JSON.stringify(this.items));
+        this.show();
+    }
+    totalPrice() { // returns total price of carted items
+        let price = 0;
+        for (const id in this.items) {
+            const item = PRODUCTS.find(v => v.id === id);
+            if (!item) return;
+            price += item.price * this.items[id];
         }
-        this.items = items;
-        const itemsStr = JSON.stringify(items);
-        localStorage.setItem('cart', itemsStr);
+        return price;
     }
-    loadItems() {
-        const items = localStorage.getItem('cart');
-        this.items = items ? JSON.parse(items) : {};
+    show() { // displays cart items
+        cartDiv.innerHTML = "";
+        for (let id in this.items) {
+            const item = PRODUCTS.find(v => v.id === id);
+            if (!item) continue;
+            const itemDiv = document.createElement('div');
+            const imgTxtHolder = document.createElement('div');
+            const imgDiv = document.createElement('img');
+            const txtDiv = document.createElement('div');
+            const rmvBtn = document.createElement('button');
+
+            itemDiv.className = "d-flex align-items-center justify-content-between";
+            rmvBtn.className = "btn cart-item-remove";
+            rmvBtn.innerHTML = "❌";
+            rmvBtn.setAttribute('item-id', item.id);
+
+            imgTxtHolder.className = 'cart-item d-flex justify-content-start align-items-center my-1 gap-3';
+            imgDiv.src = "/src/imgs/products/" + item.img;
+            imgDiv.className = "cart-img";
+            txtDiv.innerHTML = `(${this.items[id]}) - ` + item.name;
+
+            imgTxtHolder.appendChild(imgDiv);
+            imgTxtHolder.appendChild(txtDiv);
+            itemDiv.appendChild(imgTxtHolder);
+            itemDiv.appendChild(rmvBtn);
+            cartDiv.appendChild(itemDiv);
+            addRemoveListener();
+            priceDiv.innerHTML = `${this.totalPrice().toLocaleString()} BDT`;
+        }
+
+        // hide's total price and clear cart button if no items in cart
+        if (Object.keys(this.items).length <= 0) priceRmv.className = "d-none mt-3";
+        else priceRmv.className = "d-block mt-3";
     }
 }
 
-const cart = new Cart({});
-cart.loadItems();
+/* get new cart instance */
+const cart = new Cart();
+cart.show(); // load items from localStorage if any
+
+/* Adds remove listener to cart items */
+function addRemoveListener() {
+    const rmvBtns = document.querySelectorAll(".cart-item-remove");
+    for (let i = 0; i < rmvBtns.length; i++) {
+        rmvBtns[i].addEventListener('click', function () {
+            cart.remove(rmvBtns[i].getAttribute('item-id'));
+        });
+    }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,14 @@
-const PRODUCTS = [
-    { id: "UiPQXFZBKY", name: "Deepcool RF 120R", price: 6400, img: "1.jpg" },
-    { id: "WMFXRAsNof", name: "Deepcool RF 120W", price: 5400, img: "2.jpg" },
-    { id: "NfUSRmHZcu", name: "PCCooler RF 120X", price: 7300, img: "3.jpg" },
-    { id: "oIyurusmMa", name: "No Brand PC Cooler", price: 4600, img: "4.jpg" },
-    { id: "YwuNwIetjx", name: "Apple PC Fan XSX", price: 9700, img: "5.jpg" },
-];
-
-const productsDiv = document.getElementById('products');
-
-PRODUCTS.forEach(product => {
-    createProduct(product);
-});
-
-
-function createProduct({ id, name, price, img }) {
-    const mainDiv = document.createElement('div');
-    const imgElem = document.createElement('img');
-    const btnElem = document.createElement('button');
-    const h3 = document.createElement('h3');
-    h3.textContent = name;
-    btnElem.textContent = "Add to Cart";
-    btnElem.classList.add("addToCartBtn");
-    mainDiv.classList.add("product");
-    btnElem.setAttribute("product-id", id);
-    imgElem.setAttribute("src", `/src/imgs/products/${img}`);
-    mainDiv.appendChild(imgElem);
-    mainDiv.appendChild(h3);
-    mainDiv.appendChild(btnElem);
-    productsDiv.appendChild(mainDiv);
-    addClickListeners();
-}
-
+/* function for adding products in the cart */
 function addToCart(e) {
     const id = e.target.getAttribute("product-id");
-    cart.addItem(id);
+    cart.add(id);
 }
 
-function addClickListeners() {
-    const products = document.getElementsByClassName('product');
-    for (v of products) {
-        v.addEventListener('click', addToCart)
-    }
-}
+(function addClickListeners() {
+    /* add to cart listeners */
+    const products = document.getElementsByClassName('addToCartBtn');
+    for (pro of products) pro.addEventListener('click', addToCart);
+
+    /* clear cart button listener */
+    document.getElementById('clear-cart').addEventListener('click', e => cart.clear());
+})();
